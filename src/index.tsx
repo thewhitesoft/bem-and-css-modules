dd7af11fd125e462a28d3b6efdada33b568fe353
Maxim Seredkin <m.seredkin@white-soft.ru>
2021-07-26 15:40:18 +1000

    DoIt functional proptotype

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DoIt from './DoIt/DoIt';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DoIt />
   </React.StrictMode>,
   document.getElementById('root')
 );
